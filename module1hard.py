51440899d6b89f2ff77cd6d81959e380c94b90c2
AzatM302 <zell4e@gmail.com>
2024-07-23 15:43:22 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -4,6 +4,6 @@ grades = (sum(grades[0])/len(grades[0]),
           sum(grades[1])/len(grades[1]),
           sum(grades[2])/len(grades[2]),
           sum(grades[3])/len(grades[3]),
-          sum(grades[4])/len(grades[4]))# при sum(grades)/len(grades) или sum(grades[0:])/len(grades[0:]) выдает ошибку.
+          sum(grades[4])/len(grades[4]))
 a = dict(zip(sorted(students),grades))
 print(a)
